8a65a17e8e26e2e69c5cfbc7410b4b2a6b35aeae
woghd111234 <jaehong243587@gmail.com>
2024-05-07 14:04:45 +0900

    날짜데이터 형식변환

--- data_analyze.py
+++ data_analyze.py
@@ -10,9 +10,16 @@ marketing_df = pd.read_csv("Marketing.csv",encoding=encoding) #마케팅날짜, 
 onlinesales_df = pd.read_csv("Onlinesales.csv",encoding=encoding) #고객ID, 거래ID(Transaction_#####), 거래날짜, 제품ID(Product_####) 제품카테고리, 주문수량, 단위가격(원), 배송비용(원), 할인쿠폰 적용여부
 
 #로우데이터 출력
+print("고객정보")
 print(customer_df)
+print("할인정보")
 print(discount_df)
+print("마케팅정보")
 print(marketing_df)
+print("온라인판매정보")
 print(onlinesales_df)
 
 #데이터 클랜징
+#형식변환
+marketing_df["날짜"] = pd.to_datetime(data["날짜"])
+onlinesales_df["거래날짜"] = pd.to_datetime(data["거래날짜"])
