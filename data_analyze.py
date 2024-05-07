11d2842b977a727339ba73f7bf484db72759d1b6
woghd111234 <jaehong243587@gmail.com>
2024-05-07 13:02:23 +0900

    csv 파일 출력기능 추가

--- data_analyze.py
+++ data_analyze.py
@@ -6,3 +6,8 @@ customer_df = pd.read_csv("Customer.csv") #고객ID, 고객성별, 고객지역,
 discount_df = pd.read_csv("Discount.csv") #월별 정보, 제품 카테고리, 쿠폰코드, 할인률
 marketing_df = pd.read_csv("Marketing.csv") #마케팅날짜, 온/오프라인 마케팅비용(원)
 onlinesales_df = pd.read_csv("Onlinesales.csv") #고객ID, 거래ID, 거래날짜, 제품카테고리, 주문수량, 단위가격(원), 배송비용(원), 할인쿠폰 적용여부
+
+print(customer_df)
+print(discount_df)
+print(marketing_df)
+print(onlinesales_df)
