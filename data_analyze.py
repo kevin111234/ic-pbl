d969586001e68b5b0896cc7395e582205bef78c0
woghd111234 <jaehong243587@gmail.com>
2024-05-08 01:55:45 +0900

    데이터 분석을 위한 새로운 데이터프레임 생성. 데이터 클랜징 작업 포함

--- data_analyze.py
+++ data_analyze.py
@@ -13,27 +13,54 @@ onlinesales_df = pd.read_csv("Onlinesales.csv",encoding=encoding) #고객ID, 거
 # 결측값 확인
 customer_결측값_비율 = customer_df.isna().sum() / len(customer_df)
 print("고객 정보 결측값 비율:",customer_결측값_비율)
-
 discount_결측값_비율 = discount_df.isna().sum() / len(discount_df)
 print("할인 정보 결측값 비율:", discount_결측값_비율)
-
 marketing_결측값_비율 = marketing_df.isna().sum() / len(marketing_df)
 print("마케팅 정보 결측값 비율:", marketing_결측값_비율)
 online_결측값_비율 = onlinesales_df.isna().sum() / len(onlinesales_df)
 print("온라인 판매 정보 결측값 비율:", online_결측값_비율)
 
+if customer_결측값_비율.any()>0:
+    print("customer_df의 결측값이 존재합니다. 해당 행을 삭제합니다.")
+    customer_df.dropna(inplace=True)
+if discount_결측값_비율.any()>0:
+    print("discount_df의 결측값이 존재합니다. 해당 행을 삭제합니다.")
+    discount_df.dropna(inplace=True)
+if marketing_결측값_비율.any()>0:
+    print("marketing_df의 결측값이 존재합니다. 해당 행을 삭제합니다.")
+    marketing_df.dropna(inplace=True)
+if online_결측값_비율.any()>0:
+    print("onlinesales_df의 결측값이 존재합니다. 해당 행을 삭제합니다.")
+    onlinesales_df.dropna(inplace=True)
+
 # 날짜 형식변환
 marketing_df["날짜"] = pd.to_datetime(marketing_df["날짜"])
 onlinesales_df["거래날짜"] = pd.to_datetime(onlinesales_df["거래날짜"])
+
 # 할인정보 월 데이터 변환
 month_mapping = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
 discount_df["월"] = discount_df["월"].apply(lambda x: pd.to_datetime(month_mapping[x], format='%m'))
 discount_df["월"] = discount_df["월"].astype(str)
 discount_df["월"] = discount_df["월"].str[5:7].astype(int)
+
 # 칼럼명 수정(거래날짜, 마케팅정보 날짜)
 onlinesales_df.rename(columns={"거래날짜":"날짜"}, inplace=True)
+
+# 월 데이터 추출
+discount_df["월"] = discount_df["월"].astype(int)
+marketing_df["월"] = marketing_df["날짜"].astype(str)
+marketing_df["월"] = marketing_df["월"].str[5:7].astype(int)
+onlinesales_df["월"] = onlinesales_df["날짜"].astype(str)
+onlinesales_df["월"] = onlinesales_df["월"].str[5:7].astype(int)
+
 # 고객 데이터와 온라인 판매 데이터 결합
 detail_sales_df = customer_df.merge(onlinesales_df, on="고객ID")
+
+# 온라인 판매 데이터, 마케팅 데이터 결합
+detail_consum_df = onlinesales_df.merge(marketing_df, on="날짜")
+if '월_y' in detail_consum_df.columns:
+    detail_consum_df.drop("월_y", axis=1, inplace=True)  # 존재하는 경우만 삭제
+detail_consum_df.rename(columns={"월_x":"월"}, inplace=True)
 
 # 클랜징 후 데이터 출력
 print("고객정보")
@@ -46,10 +73,12 @@ print("온라인판매정보")
 print(onlinesales_df)
 print("고객정보 + 온라인판매정보")
 print(detail_sales_df)
+print("온라인판매정보+마케팅정보")
+print(detail_consum_df)
 
 # sql에 데이터를 저장할지 입력
 while True:
-    sql_save = input("데이터를 DB에 저장하겠습니까?(y/n): ")
+    sql_save = input("데이터를 DB에 저장하시겠습니까?(y/n): ")
     if sql_save=="y":
         print("데이터를 DB에 저장합니다")
         break
