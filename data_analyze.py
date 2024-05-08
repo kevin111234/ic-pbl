54941e9b142381cb7bbccad79302568e49c679be
woghd111234 <jaehong243587@gmail.com>
2024-05-08 16:39:57 +0900

    데이터 클랜징 함수 구현

--- data_analyze.py
+++ data_analyze.py
@@ -6,35 +6,32 @@ import data_save
 import data_read
 
 # 로우데이터 읽기
-encoding = "cp949"
-customer_df = pd.read_csv("Customer.csv",encoding=encoding) #고객ID(USER_####), 고객성별(남/여), 고객지역, 가입기간(월)
-discount_df = pd.read_csv("Discount.csv",encoding=encoding) #월별 정보(JAN 등으로 표시), 제품 카테고리(불규칙적), 쿠폰코드, 할인률(%)
-marketing_df = pd.read_csv("Marketing.csv",encoding=encoding) #마케팅날짜, 온/오프라인 마케팅비용(원)
-onlinesales_df = pd.read_csv("Onlinesales.csv",encoding=encoding) #고객ID, 거래ID(Transaction_#####), 거래날짜, 제품ID(Product_####) 제품카테고리, 주문수량, 단위가격(원), 배송비용(원), 할인쿠폰 적용여부
+customer_df = data_read.csv_read("Customer.csv") #고객ID(USER_####), 고객성별(남/여), 고객지역, 가입기간(월)
+discount_df = data_read.csv_read("Discount.csv") #월별 정보(JAN 등으로 표시), 제품 카테고리(불규칙적), 쿠폰코드, 할인률(%)
+marketing_df = data_read.csv_read("Marketing.csv") #마케팅날짜, 온/오프라인 마케팅비용(원)
+onlinesales_df = data_read.csv_read("Onlinesales.csv") #고객ID, 거래ID(Transaction_#####), 거래날짜, 제품ID(Product_####) 제품카테고리, 주문수량, 단위가격(원), 배송비용(원), 할인쿠폰 적용여부
 
 # 데이터 클랜징
-for i in [customer_df, discount_df, marketing_df, onlinesales_df]:
-    data_read.na_cleaning(i)
+df_list = [customer_df, discount_df, marketing_df, onlinesales_df]
+df_name_list = ["customer_df", "discount_df", "marketing_df", "onlinesales_df"]
+for i in df_list:
+    data_read.na_cleaning(i)    
 
 # 날짜 형식변환
-marketing_df["날짜"] = pd.to_datetime(marketing_df["날짜"])
-onlinesales_df["거래날짜"] = pd.to_datetime(onlinesales_df["거래날짜"])
+data_read.to_date(marketing_df, "날짜")
+data_read.to_date(onlinesales_df,"거래날짜")
 
 # 할인정보 월 데이터 변환 -> <<<월 데이터 추출하는 함수 만들기!>>>
 month_mapping = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
 discount_df["월"] = discount_df["월"].apply(lambda x: pd.to_datetime(month_mapping[x], format='%m'))
-discount_df["월"] = discount_df["월"].astype(str)
-discount_df["월"] = discount_df["월"].str[5:7].astype(int)
+data_read.month_mapping(discount_df,"월")
 
 # 칼럼명 수정(거래날짜, 마케팅정보 날짜)
 onlinesales_df.rename(columns={"거래날짜":"날짜"}, inplace=True)
 
 # 월 데이터 추출 -> <<<월 데이터 추출하는 함수 만들기!>>>
-discount_df["월"] = discount_df["월"].astype(int)
-marketing_df["월"] = marketing_df["날짜"].astype(str)
-marketing_df["월"] = marketing_df["월"].str[5:7].astype(int)
-onlinesales_df["월"] = onlinesales_df["날짜"].astype(str)
-onlinesales_df["월"] = onlinesales_df["월"].str[5:7].astype(int)
+data_read.month_mapping(marketing_df,"날짜")
+data_read.month_mapping(onlinesales_df,"날짜")
 
 # 고객 데이터와 온라인 판매 데이터 결합
 detail_sales_df = customer_df.merge(onlinesales_df, on="고객ID")
--- data_read.py
+++ data_read.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
+# 로우데이터 읽기
+def csv_read(csv_file):
+    try:
+        df = pd.read_csv(csv_file, encoding="cp949")
+        return(df)
+    except FileNotFoundError:
+        print(f"오류: 파일을 찾을 수 없습니다 - {csv_file}")
+        return None
+    except pd.errors.ParserError:
+        print(f"오류: CSV 파일 파싱 오류 - {csv_file}")
+        return None
+
+
 # 데이터 클랜징
 # 결측값 확인
-def na_cleaning(dataframe):
+def na_cleaning(dataframe, fillna_strategy=None, threshold=None):
     결측값_비율 = dataframe.isna().sum() / len(dataframe)
     print("결측값 비율:", 결측값_비율)
     if 결측값_비율.any()>0:
@@ -10,5 +23,11 @@ def na_cleaning(dataframe):
         dataframe.dropna(inplace=True)
 
 #날짜 형식 변환
+def to_date(dataframe, column):
+    dataframe[column] = pd.to_datetime(dataframe[column])
 
 #월 데이터 추출
+def month_mapping(dataframe,column):
+    dataframe["월"] = dataframe[column].astype(str)
+    dataframe["월"] = dataframe["월"].str[5:7].astype(int)
+    dataframe["월"] = dataframe["월"].astype(int)
