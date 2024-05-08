e3400c76b4eb5a54866098cc4289ab84b4fa62f2
woghd111234 <jaehong243587@gmail.com>
2024-05-09 06:36:11 +0900

    데이터 불러오기 함수 생성, sqlalchemy 모듈을 활용하는 엔진 불러오기 함수 생성

--- data_visualize.py
+++ data_visualize.py
@@ -1,8 +1,20 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import pymysql
+
+import data_save
 
 # SQL에서 데이터 들고오기
-
+sql_pswd = input("SQL 비밀번호를 입력해주세요: ")
+engine,query = data_save.db_pull_out("customer_info", sql_pswd)
+customer_df = pd.read_sql(query, engine)
+engine,query = data_save.db_pull_out("discount_info", sql_pswd)
+discount_df = pd.read_sql(query, engine)
+engine,query = data_save.db_pull_out("marketing_info", sql_pswd)
+marketing_df = pd.read_sql(query, engine)
+engine,query = data_save.db_pull_out("onlinesales_info", sql_pswd)
+onlisesales_df = pd.read_sql(query, engine)
+engine.dispose()
 
 # SQL에서 데이터 결합해서 들고오기
 
