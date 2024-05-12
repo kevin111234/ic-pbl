19412682a7bab510695bde655000b4aa690e7ee5
woghd111234 <jaehong243587@gmail.com>
2024-05-13 07:27:47 +0900

    성별에 따른 카테고리별 구매량 비교 시각화

--- data_visualize.py
+++ data_visualize.py
@@ -70,58 +70,13 @@ engine,query = data_output.db_group_1column("discount_info.`할인율`, SUM(onli
                                             ,"GROUP BY discount_info.`할인율` ORDER BY 할인율 ASC")
 rate_discount_df = pd.read_sql(query, engine)
 """
-"""
-# 확인용 print문
-# 기본 테이블
-print(customer_df)
-print(discount_df)
-print(marketing_df)
-print(onlisesales_df)
 
-# 2테이블 결합
-print("고객정보 판매정보 결합")
-print(customer_onlinesales_df)
-print("할인정보 판매정보 결합")
-print(discount_onlinesales_df)
-print("마케팅정보 판매정보 결합")
-print(marketing_onlinesales_df)
-
-# 세부결합 테이블
-print("카테고리별 고객 구매정보")
-print(individual_customer_df)
-print("월별 고객 구매정보")
-print(month_customer_df)
-print("고객지역별 카테고리 구매정보")
-print(local_customer_df)
-print("고객지역별 구매정보")
-print(local_df)
-print("가입기간별 카테고리 구매정보")
-print(period_customer_df)
-print("성별별 카테고리 구매정보")
-print(gender_customer_df)
-print("성별별 구매정보")
-print(gender_df)
-print("월별 구매정보")
-print(month_discount_df)
-print("할인율별 구매정보")
-print(rate_discount_df)
-"""
-# 1. 고객별 선호제품 경향 파악
-# 사용자로부터 입력받은 카테고리 목록
+# 카테고리 목록, 지역 목록
 categories = ["Nest-USA","Office","Apparel","Drinkware","Notebooks & Journals","Waze","Fun","Headgear","Lifestyle","Nest-Canada","Bags",
                 "Gift Cards","Android","Bottles","Backpacks","Google","Housewares","Accessories","Nest","More Bags"]
-# 동적 SQL 쿼리 생성
-query = """SELECT customer_info.고객ID, {}
-FROM customer_info JOIN onlinesales_info ON customer_info.고객ID=onlinesales_info.고객ID
-GROUP BY customer_info.고객ID;
-"""
-# 각 카테고리에 대한 CASE 문 생성
-case_statements = ",\n".join([f"(SUM(CASE WHEN 제품카테고리 = '{category}' THEN 1 ELSE 0 END)/COUNT(*))*100 AS '{category.lower()}비율'" for category in categories])
-# 쿼리 문자열에 CASE 문 삽입
-query = query.format(case_statements)
-individual_count_df = pd.read_sql(query, engine)
+regions = ["Chicago", "California", "NewYork", "New Jersey", "Washington DC"]
 
-# 2.지역별 선호제품 경향 파악
+# 1.지역별 선호제품 경향 파악
 # 동적 SQL 쿼리 생성
 query = """SELECT customer_info.고객지역, {}
 FROM customer_info JOIN onlinesales_info ON customer_info.고객ID=onlinesales_info.고객ID
@@ -133,6 +88,12 @@ case_statements = ",\n".join([f"(SUM(CASE WHEN 제품카테고리 = '{category}'
 query = query.format(case_statements)
 local_count_df = pd.read_sql(query, engine)
 
+# 2. 성별에 따른 시장규모 비교
+engine,query = data_output.db_group_1column("customer_info.성별, COUNT(*)AS 고객_수, SUM(평균금액*수량+배송료)AS 구매금액, SUM(수량)AS 수량"
+                                            ,"customer_info", "onlinesales_info", "고객ID", sql_pswd
+                                            ,"GROUP BY customer_info.성별 ORDER BY 구매금액 DESC")
+gender_df = pd.read_sql(query, engine)
+
 # 3. 가입기간별 구입추이 경향 파악
 # 가입기간별 카테고리 구매정보
 engine,query = data_output.db_group_1column("customer_info.가입기간, SUM(평균금액*수량+배송료)AS 구매금액, SUM(수량)AS 수량"
@@ -140,19 +101,19 @@ engine,query = data_output.db_group_1column("customer_info.가입기간, SUM(평
                                             ,"GROUP BY customer_info.가입기간 ORDER BY 가입기간 ASC")
 period_customer_df = pd.read_sql(query, engine)
 
-# 6. 성별에 따른 시장규모 비교
-engine,query = data_output.db_group_1column("customer_info.성별, COUNT(*)AS 고객_수, SUM(평균금액*수량+배송료)AS 구매금액, SUM(수량)AS 수량"
+# 4. 성별에 따른 카테고리별 (평균) 구매량 비교
+engine,query = data_output.db_group_1column("customer_info.`성별`, onlinesales_info.`제품카테고리`,COUNT(*)AS 고객수, SUM(onlinesales_info.`수량`)AS 수량, SUM(평균금액*수량+배송료)AS 구매금액, (SUM(onlinesales_info.`수량`)/COUNT(*))AS 평균수량,(SUM(평균금액*수량+배송료)/COUNT(*))AS 평균구매액"
                                             ,"customer_info", "onlinesales_info", "고객ID", sql_pswd
-                                            ,"GROUP BY customer_info.성별 ORDER BY 구매금액 DESC")
-gender_df = pd.read_sql(query, engine)
+                                            ,'''WHERE 성별="남"GROUP BY customer_info.`성별`, onlinesales_info.`제품카테고리`''')
+male_customer_df = pd.read_sql(query, engine)
+engine,query = data_output.db_group_1column("customer_info.`성별`, onlinesales_info.`제품카테고리`,COUNT(*)AS 고객수, SUM(onlinesales_info.`수량`)AS 수량, SUM(평균금액*수량+배송료)AS 구매금액, (SUM(onlinesales_info.`수량`)/COUNT(*))AS 평균수량,(SUM(평균금액*수량+배송료)/COUNT(*))AS 평균구매액"
+                                            ,"customer_info", "onlinesales_info", "고객ID", sql_pswd
+                                            ,'''WHERE 성별="여"GROUP BY customer_info.`성별`, onlinesales_info.`제품카테고리`''')
+female_customer_df = pd.read_sql(query, engine)
+
 
 # 분석 결과 시각화
-# 1. 고객별 선호제품 경향 파악 - individual_count_df(데이터 양이너무 많아서 시각화 빡셈. 그룹으로 더 묶어서 확인하기.)
-#for category in categories:
-#    plt.plot(individual_count_df[f"{category.lower()}비율"],individual_count_df["고객ID"])
-
-# 2. 지역별 선호제품 경향 파악 - local_count_df
-regions = ["Chicago", "California", "NewYork", "New Jersey", "Washington DC"]
+# 1. 지역별 선호제품 경향 파악 - local_count_df
 fig, axes = plt.subplots(2, 2, figsize=(15, 18), gridspec_kw={'height_ratios': [2, 2]}) 
 
 # 첫 번째 서브플롯: 고객 지역별 상품 구매 비율
@@ -174,7 +135,7 @@ axes[0, 1].set_xlabel("성별")
 axes[0, 1].set_ylabel("구매금액")
 axes[0, 1].set_title("성별에 따른 구매금액 (단위: 억 원)")
 for i, value in enumerate(gender_df["구매금액"]):
-    axes[1, 1].text(i, value + 1, str(value), ha='center')
+    axes[0, 1].text(i, value + 1, str(round(value/1000000000,3)), ha='center')
 
 # 세 번째 서브플롯: 가입기간별 구매금액
 axes[1, 0].plot(period_customer_df["가입기간"], period_customer_df["구매금액"], color="blue")
@@ -188,5 +149,56 @@ axes[1, 1].set_xlabel("가입기간")
 axes[1, 1].set_ylabel("구매 수량")
 axes[1, 1].set_title("가입기간별 구매 수량")
 
-plt.tight_layout(pad=1.0)  # 서브플롯 간 간격 조절
+plt.tight_layout()  # 서브플롯 간 간격 조절
+plt.show()
+
+# 다섯 번째 서브플롯: 성별에 따른 평균 구매 수량
+categories = male_customer_df["제품카테고리"]
+
+# 그래프를 위한 데이터 준비
+quantity_male = male_customer_df["수량"]
+quantity_female = female_customer_df["수량"]
+purchase_amount_male = male_customer_df["구매금액"]
+purchase_amount_female = female_customer_df["구매금액"]
+average_quantity_male = male_customer_df["평균수량"]
+average_quantity_female = female_customer_df["평균수량"]
+average_purchase_amount_male = male_customer_df["평균구매액"]
+average_purchase_amount_female = female_customer_df["평균구매액"]
+
+# 서브플롯 설정
+fig, axes = plt.subplots(2, 2, figsize=(15, 10))
+
+# 수량 그래프
+axes[0, 0].bar(categories, quantity_male, label="Male", color="blue", alpha=0.5)
+axes[0, 0].bar(categories, quantity_female, label="Female", color="pink", alpha=0.5)
+axes[0, 0].set_title("카테고리별 구매량")
+axes[0, 0].set_ylabel("구매량")
+axes[0, 0].legend()
+axes[0, 0].tick_params(axis='x', rotation=90)  # x축 라벨 90도 회전
+
+# 구매금액 그래프
+axes[0, 1].bar(categories, purchase_amount_male, label="Male", color="blue", alpha=0.5)
+axes[0, 1].bar(categories, purchase_amount_female, label="Female", color="pink", alpha=0.5)
+axes[0, 1].set_title("카테고리별 구매금액")
+axes[0, 1].set_ylabel("구매금액")
+axes[0, 1].legend()
+axes[0, 1].tick_params(axis='x', rotation=90)  # x축 라벨 90도 회전
+
+# 평균수량 그래프
+axes[1, 0].bar(categories, average_quantity_male, label="Male", color="blue", alpha=0.5)
+axes[1, 0].bar(categories, average_quantity_female, label="Female", color="pink", alpha=0.5)
+axes[1, 0].set_title("고객별 평균 구매 수량")
+axes[1, 0].set_ylabel("평균 구매 수량")
+axes[1, 0].legend()
+axes[1, 0].tick_params(axis='x', rotation=90)  # x축 라벨 90도 회전
+
+# 평균구매액 그래프
+axes[1, 1].bar(categories, average_purchase_amount_male, label="Male", color="blue", alpha=0.5)
+axes[1, 1].bar(categories, average_purchase_amount_female, label="Female", color="pink", alpha=0.5)
+axes[1, 1].set_title("고객별 평균 구매 금액")
+axes[1, 1].set_ylabel("평균 구매")
+axes[1, 1].legend()
+axes[1, 1].tick_params(axis='x', rotation=90)  # x축 라벨 90도 회전
+
+plt.tight_layout()
 plt.show()
