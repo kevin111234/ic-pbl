1e2444fb40bebdf01e510917bccfd2f33ee08b80
woghd111234 <jaehong243587@gmail.com>
2024-05-23 07:41:14 +0900

    필요없는 파일 삭제, 고객분류 코드 파일 생성

---
+++ Customer_Segment.py
@@ -0,0 +1,17 @@
+import data_frame
+
+#기본 데이터 로드
+sql_pswd = input("SQL 비밀번호를 입력해주세요: ")
+data_frame.customer(sql_pswd)
+data_frame.marketing(sql_pswd)
+data_frame.discount(sql_pswd)
+data_frame.onlinesales(sql_pswd)
+category_df = data_frame.category_df
+marketing_df = data_frame.marketing_df
+discount_df = data_frame.discount_df
+onlinesales_df = data_frame.onlinesales_df
+
+#데이터 전처리
+#ID 형식들 숫자로 변환
+#텍스트데이터 벡터로 변환
+#코사인유사도 계산
