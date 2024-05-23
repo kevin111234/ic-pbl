25d52b9d77ff5777f868cf39cb617d9869eb2660
woghd111234 <jaehong243587@gmail.com>
2024-05-23 23:07:26 +0900

    코드 순서 수정, 주석 수정

--- Customer_Segment.py
+++ Customer_Segment.py
@@ -13,19 +13,21 @@ discount_df = data_frame.discount_df
 onlinesales_df = data_frame.onlinesales_df
 
 #데이터 전처리
-#ID 형식들 숫자로 변환
+    #ID 형식들 숫자로 변환
 customer_df['고객ID'] = customer_df['고객ID'].astype(str).str[5:]
 customer_df['고객ID'] = customer_df['고객ID'].astype(int)
 onlinesales_df['고객ID'] = onlinesales_df['고객ID'].astype(str).str[5:]
 onlinesales_df['고객ID'] = onlinesales_df['고객ID'].astype(int)
 onlinesales_df['제품ID'] = onlinesales_df['제품ID'].astype(str).str[8:]
-onlinesales_df['제품ID'] = onlinesales_df['제품카테고리']+" "+onlinesales_df['제품ID']
 onlinesales_df['거래ID'] = onlinesales_df['거래ID'].astype(str).str[12:]
 onlinesales_df['거래ID'] = onlinesales_df['거래ID'].astype(int)
+    #제품ID랑 카테고리 연결
+onlinesales_df['제품ID'] = onlinesales_df['제품카테고리']+" "+onlinesales_df['제품ID']
 
-#제품ID랑 카테고리 연결
 #텍스트데이터 벡터로 변환 (제품카테고리, 고객지역)
-#필요시 원-핫 인코딩
+
+#필요시 원-핫 인코딩(성별)
+
 #코사인유사도 계산
 
 #모델 형성
